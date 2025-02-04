7ee7c0bfbb7b5dd382af84f9352bc83d42e1e12c
Divyanshu Kulshreshtha <101164039+DivyanshuKuls@users.noreply.github.com>
2025-02-04 17:24:02 +0000

    create linked list

---
+++ linked-list/create-linked-list.js
@@ -0,0 +1,30 @@
+// Function to create singly linked list
+class node {
+    constructor (data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+const createLinkedList = (n) => {
+    let head = null;
+
+    for(let i=1; i<=n; i++) {
+        let newNode = new node(i);
+
+        if(head === null) {
+            head = newNode;
+        } else {
+            let temp = head;
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
+        }
+    }
+
+    return head;
+}
+
+console.log(createLinkedList(2));
